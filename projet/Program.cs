d366f0cc6bde86a8b5276346bfc208c464415b4d
cyril-renevey <33549432+cyril-renevey@users.noreply.github.com>
2017-11-10 16:44:06 +0100

    Projet Vanity adress proof of work

---
+++ projet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NBitcoin;
+
+namespace MyProject
+{
+    class Program
+    {
+        public static void Main()
+        {
+            
+            int N = 5;
+            int maxN = 100000;
+            string test = " ";
+            string initial = " ";
+            string text = "mummy";
+            int i = 0;
+            Key trial = new Key();
+            while (i < maxN && initial.ToLower() != text.Substring(0,N))
+            {
+                trial = new Key();
+                var address = trial.PubKey.GetAddress(Network.TestNet);
+                test = address.ToString();
+                initial = test.Substring(0,N);
+                i++;
+            }
+            Console.WriteLine("Adress : " + test);
+            Console.WriteLine("Sequence : " + initial);
+            Console.WriteLine("Private key : " + trial.GetBitcoinSecret(Network.TestNet));
+        }
+    }
+}
